3ea732251a8c3046d7f55098a0dae0234ab7e9c8
Katie <mccann.ek@gmail.com>
2017-12-07 19:20:03 -0500

    Create day-5.js

---
+++ day-5.js
@@ -0,0 +1,19 @@
+const input = require('./aoc-day-5.js');
+
+//Process the input into a single array
+const processInput = (input)=>{
+    return input.split('\n').map((item)=>Number(item));
+};
+
+//Given a number, move that many numbers up or down in the array
+const jumpSpaces = (numberArray, countMoves=0, index=0)=>{
+	while (index < numberArray.length){
+		let oldIndex = index;
+		index = index + numberArray[index];
+		numberArray[oldIndex] += 1;
+		countMoves += 1;
+	};
+	return countMoves;
+};
+
+console.log(jumpSpaces(processInput(aocInput)));
